5eb3582fb7329fcf5d27fccfe1d047c7554d5842
Dhruv-Mahajan1 <23434514-Dhruv-Mahajan1@users.noreply.replit.com>
2023-05-28 10:37:58 +0000

    Initial commit

---
+++ django_project/views.py
@@ -0,0 +1,44 @@
+# views.py
+from django.http import HttpResponseRedirect
+from django.views import View
+from google_auth_oauthlib.flow import Flow
+from googleapiclient.discovery import build
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+CLIENT_SECRET_FILE = 'django_project/client_secret.json'
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+API_SERVICE_NAME = 'calendar'
+API_VERSION = 'v3'
+
+
+class GoogleCalendarInitView(View):
+    def get(self, request):
+        flow = Flow.from_client_secrets_file(CLIENT_SECRET_FILE, scopes=SCOPES)
+        flow.redirect_uri = request.build_absolute_uri(
+            '/rest/v1/calendar/redirect/')
+        authorization_url, state = flow.authorization_url(
+            access_type='offline', include_granted_scopes='true')
+        request.session['oauth_state'] = state
+        return HttpResponseRedirect(authorization_url)
+
+
+class GoogleCalendarRedirectView(APIView):
+    def get(self, request):
+        state = request.session.pop('oauth_state', '')
+        flow = Flow.from_client_secrets_file(CLIENT_SECRET_FILE,
+                                             scopes=SCOPES,
+                                             state=state)
+        flow.redirect_uri = request.build_absolute_uri(
+            '/rest/v1/calendar/redirect/')
+        authorization_response = request.build_absolute_uri()
+        flow.fetch_token(authorization_response=authorization_response)
+        credentials = flow.credentials
+        service = build(API_SERVICE_NAME,
+                        API_VERSION,
+                        credentials=credentials,
+                        static_discovery=False)
+        events_result = service.events().list(calendarId='primary',
+                                              maxResults=10).execute()
+        events = events_result.get('items', [])
+        return Response(events)
